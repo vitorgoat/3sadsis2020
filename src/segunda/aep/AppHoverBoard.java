1f3d332564290be07f90a4e2569e2ddab604c336
Vitor Brito <60374775+vitorgoat@users.noreply.github.com>
2020-04-23 22:43:08 -0300

    Update AppHoverBoard.java

--- src/segunda/aep/AppHoverBoard.java
+++ src/segunda/aep/AppHoverBoard.java
@@ -3,11 +3,11 @@ package segunda.aep;
 public class AppHoverBoard {
 
 	public static void main(String[] args0) {
-		HoverBoard HoverBoard1 = new HoverBoard("D-EDGEX", 87542, 92);
+		HoverBoard hoverBoard1 = new HoverBoard("D-EDGEX", 87542, 92);
 
 		HoverBoard1.verificarSituacaoDoHover();
 
-		HoverBoard HoverBoard2 = new HoverBoard("LBS9001", 22544, 29);
+		HoverBoard hoverBoard2 = new HoverBoard("LBS9001", 22544, 29);
 		
 		HoverBoard2.bloquearHover();
 		HoverBoard2.verificarSituacaoDoHover();
